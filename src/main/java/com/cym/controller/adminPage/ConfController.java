56fb33299553b1769f207a088ea4055e5a29e13f
cym1102 <cym1102@qq.com>
2020-04-23 17:15:48 +0800

    1

--- src/main/java/com/cym/controller/adminPage/ConfController.java
+++ src/main/java/com/cym/controller/adminPage/ConfController.java
@@ -1,10 +1,13 @@
 package com.cym.controller.adminPage;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,18 +16,22 @@ import org.springframework.web.servlet.ModelAndView;
 import com.cym.model.Http;
 import com.cym.model.Server;
 import com.cym.utils.BaseController;
+import com.github.odiszapc.nginxparser.NgxBlock;
+import com.github.odiszapc.nginxparser.NgxConfig;
+import com.github.odiszapc.nginxparser.NgxDumper;
+import com.github.odiszapc.nginxparser.NgxParam;
 
 @Controller
 @RequestMapping("/adminPage/conf")
 public class ConfController extends BaseController {
-	
+
 	@RequestMapping("")
 	public ModelAndView index(HttpSession httpSession, ModelAndView modelAndView) throws IOException {
 		Http http = mongoHelper.findOneByQuery(new Criteria(), Http.class);
 		modelAndView.addObject("http", http);
 		List<Server> servers = mongoHelper.findAll(Server.class);
 		modelAndView.addObject("servers", servers);
-		
+
 		String confStr = buildConf(http, servers);
 		modelAndView.addObject("confStr", confStr);
 		modelAndView.setViewName("/adminPage/conf/index");
@@ -32,9 +39,110 @@ public class ConfController extends BaseController {
 	}
 
 	private String buildConf(Http http, List<Server> servers) {
-	
-		
-		
-		return "";
+
+		try {
+			ClassPathResource resource = new ClassPathResource("nginx.conf");
+			InputStream inputStream = resource.getInputStream();
+			
+			NgxConfig ngxConfig = NgxConfig.read(inputStream);
+			// 设置http
+			NgxBlock ngxBlockHttp = ngxConfig.findBlock("http");
+			NgxParam ngxParam = new NgxParam();
+			ngxParam.addValue("gzip " + http.getGzip());
+			ngxBlockHttp.addEntry(ngxParam);
+
+			ngxParam = new NgxParam();
+			ngxParam.addValue("client_max_body_size " + http.getClientMaxBodySize());
+			ngxBlockHttp.addEntry(ngxParam);
+			
+			// 添加server
+			for (Server server : servers) {
+				NgxBlock ngxBlockServer = new NgxBlock();
+				ngxBlockServer.addValue("server");
+
+				ngxParam = new NgxParam();
+				ngxParam.addValue("server_name " + server.getServerName());
+				ngxBlockServer.addEntry(ngxParam);
+
+				ngxParam = new NgxParam();
+				String value = "listen " + server.getListen();
+				if (server.getSsl()) {
+					value += " ssl";
+				}
+				ngxParam.addValue(value);
+				ngxBlockServer.addEntry(ngxParam);
+
+				if (server.getSsl()) {
+					ngxParam = new NgxParam();
+					ngxParam.addValue("ssl_certificate " + server.getPem());
+					ngxBlockServer.addEntry(ngxParam);
+
+					ngxParam = new NgxParam();
+					ngxParam.addValue("ssl_certificate_key " + server.getKey());
+					ngxBlockServer.addEntry(ngxParam);
+				}
+				
+				// 添加location
+				NgxBlock ngxBlockLocation = new NgxBlock();
+				ngxBlockLocation.addValue("location");
+				ngxBlockLocation.addValue("/");
+
+				ngxParam = new NgxParam();
+				ngxParam.addValue("proxy_pass http://" + server.getProxyPass() + ":" + server.getProxyPassPort());
+				ngxBlockLocation.addEntry(ngxParam);
+
+				ngxParam = new NgxParam();
+				ngxParam.addValue("proxy_set_header Host $host");
+				ngxBlockLocation.addEntry(ngxParam);
+
+				ngxParam = new NgxParam();
+				ngxParam.addValue("proxy_set_header X-Real-IP $remote_addr");
+				ngxBlockLocation.addEntry(ngxParam);
+
+				ngxParam = new NgxParam();
+				ngxParam.addValue("proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for");
+				ngxBlockLocation.addEntry(ngxParam);
+				
+				ngxBlockServer.addEntry(ngxBlockLocation);
+
+				ngxBlockHttp.addEntry(ngxBlockServer);
+				
+				// ssl添加80端口转跳
+				if (server.getSsl()) {
+					ngxBlockServer = new NgxBlock();
+					ngxBlockServer.addValue("server");
+
+					ngxParam = new NgxParam();
+					ngxParam.addValue("server_name " + server.getServerName());
+					ngxBlockServer.addEntry(ngxParam);
+
+					ngxParam = new NgxParam();
+					ngxParam.addValue("listen 80");
+					ngxBlockServer.addEntry(ngxParam);
+					
+					ngxParam = new NgxParam();
+					ngxParam.addValue("rewrite ^(.*)$ https://${server_name}$1 permanent");
+					ngxBlockServer.addEntry(ngxParam);
+					
+					ngxBlockHttp.addEntry(ngxBlockServer);
+				}
+			}
+
+			return new NgxDumper(ngxConfig).dump();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
 	}
+
+//	public static void main(String[] args) {
+//		try {
+//			NgxConfig ngxConfig = NgxConfig.read(new ByteArrayInputStream("http{}".getBytes("UTF-8")));
+//
+//			System.out.println(new NgxDumper(ngxConfig).dump());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 }
