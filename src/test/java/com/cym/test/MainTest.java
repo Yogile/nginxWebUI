e92481fa7f2b3f1499a6ed818f5d18bfb337dfe1
cym1102 <cym1102@qq.com>
2020-04-27 20:55:09 +0800

    1

--- src/test/java/com/cym/test/MainTest.java
+++ src/test/java/com/cym/test/MainTest.java
@@ -1,19 +1,26 @@
 package com.cym.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.cym.NginxWebUI;
+import com.cym.model.Admin;
+
+import cn.craccd.sqlHelper.utils.SqlHelper;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = NginxWebUI.class)
 public class MainTest {
-
-	
+	@Autowired
+	SqlHelper sqlHelper;
 	@Before
 	public void before() {
 		System.out.println("--------------测试开始----------");
@@ -21,7 +28,16 @@ public class MainTest {
 
 	@Test
 	public void testStartUp()  {
-	
+		List<Admin> list = new ArrayList<Admin>();
+		for(int i=0;i<10;i++) {
+			Admin admin = new Admin();
+			admin.setName("admin");
+			admin.setPass("admin");
+			
+			list.add(admin);
+		}
+		sqlHelper.insertAll(list);
+		
 	}
 
 	@After
