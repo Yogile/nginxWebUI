c9617164aca75aa4158fa0680e608a3cd46c3fe3
cym1102 <cym1102@qq.com>
2020-05-18 14:55:14 +0800

    1

--- src/main/java/com/cym/controller/adminPage/LoginController.java
+++ src/main/java/com/cym/controller/adminPage/LoginController.java
@@ -11,10 +11,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.cym.model.Credit;
+import com.cym.model.Remote;
 import com.cym.service.AdminService;
 import com.cym.service.CreditService;
 import com.cym.utils.BaseController;
 import com.cym.utils.JsonResult;
+
+import cn.hutool.core.util.StrUtil;
 
 /**
  * 登录页
@@ -22,7 +25,7 @@ import com.cym.utils.JsonResult;
  * @author Administrator
  *
  */
-@RequestMapping("/adminPage")
+@RequestMapping("/adminPage/login")
 @Controller
 public class LoginController extends BaseController {
 	@Autowired
@@ -30,20 +33,19 @@ public class LoginController extends BaseController {
 	@Autowired
 	CreditService creditService;
 
-	@RequestMapping("login")
+	@RequestMapping("")
 	public ModelAndView admin(ModelAndView modelAndView) {
 		modelAndView.setViewName("/adminPage/login/index");
 		return modelAndView;
 	}
 
-	@RequestMapping("login/login")
+	@RequestMapping("login")
 	@ResponseBody
 	public JsonResult submitLogin(String name, String pass, HttpSession httpSession) {
 
 		if (adminService.login(name, pass)) {
 
 			httpSession.setAttribute("localType", "本地");
-			httpSession.removeAttribute("remote");
 			httpSession.setAttribute("isLogin", true);
 			return renderSuccess();
 		} else {
@@ -51,13 +53,14 @@ public class LoginController extends BaseController {
 		}
 	}
 
-	@RequestMapping("login/loginOut")
+	@RequestMapping("loginOut")
 	public String loginOut(HttpSession httpSession) {
 		httpSession.removeAttribute("isLogin");
 		return "redirect:/adminPage/login";
 	}
 
-	@RequestMapping("login/getCredit")
+	@ResponseBody
+	@RequestMapping("getCredit")
 	public JsonResult getCredit(String name, String pass) {
 		if (adminService.login(name, pass)) {
 			String key = creditService.make();
@@ -66,6 +69,22 @@ public class LoginController extends BaseController {
 			return renderError("授权失败");
 		}
 
+	}
+
+	@ResponseBody
+	@RequestMapping("getLocalType")
+	public JsonResult getLocalType(HttpSession httpSession) {
+		String localType = (String) httpSession.getAttribute("localType");
+		if (StrUtil.isNotEmpty(localType)) {
+			if (localType.equals("本地")) {
+				return renderSuccess("本地");
+			} else {
+				Remote remote = (Remote) httpSession.getAttribute("remote");
+				return renderSuccess(remote.getIp() + ":" + remote.getPort());
+			}
+		}
+		
+		return renderSuccess("");
 	}
 
 }
