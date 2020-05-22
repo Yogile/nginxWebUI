154d0d852e295484e4f22b40b9171ead0a023a44
cym1102 <cym1102@qq.com>
2020-05-22 10:07:36 +0800

    1

--- src/main/java/com/cym/service/HttpService.java
+++ src/main/java/com/cym/service/HttpService.java
@@ -1,5 +1,7 @@
 package com.cym.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,16 +14,33 @@ import cn.craccd.sqlHelper.bean.Sort;
 import cn.craccd.sqlHelper.bean.Sort.Direction;
 import cn.craccd.sqlHelper.utils.CriteriaAndWrapper;
 import cn.craccd.sqlHelper.utils.SqlHelper;
+import cn.hutool.core.util.StrUtil;
 
 @Service
 public class HttpService {
 	@Autowired
 	SqlHelper sqlHelper;
-	
 
 	public boolean hasName(String name) {
 		return sqlHelper.findCountByQuery(new CriteriaAndWrapper().eq("name", name), Http.class) > 0;
 	}
-	
-	
+
+	public void setAll(List<Http> https) {
+		for (Http http : https) {
+			Http httpOrg = sqlHelper.findOneByQuery(new CriteriaAndWrapper().eq("name", http.getName()), Http.class);
+
+			if (httpOrg != null) {
+				http.setId(httpOrg.getId());
+			}
+
+			if (http.getName().equals("gzip") && StrUtil.isEmpty(http.getValue())) {
+				http.setValue("off");
+			}
+
+			sqlHelper.insertOrUpdate(http);
+
+		}
+
+	}
+
 }
