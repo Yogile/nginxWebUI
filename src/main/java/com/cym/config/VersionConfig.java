e9ed85455a26191fd438cac0e6925a98f6f61688
cym1102 <cym1102@qq.com>
2020-05-25 11:41:33 +0800

    1

--- src/main/java/com/cym/config/VersionConfig.java
+++ src/main/java/com/cym/config/VersionConfig.java
@@ -1,7 +1,5 @@
 package com.cym.config;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,12 +12,8 @@ import cn.hutool.json.JSONUtil;
 @Configuration
 public class VersionConfig {
 
-	@Value("${spring.application.name}")
-	String projectName;
-
 	Version version;
 
-	@PostConstruct
 	public void getNewVersion() {
 		try {
 			String json = HttpUtil.get("http://craccd.oss-cn-beijing.aliyuncs.com/version.json", 1000);
@@ -32,14 +26,6 @@ public class VersionConfig {
 
 	}
 
-	public String getProjectName() {
-		return projectName;
-	}
-
-	public void setProjectName(String projectName) {
-		this.projectName = projectName;
-	}
-
 	public Version getVersion() {
 		return version;
 	}
@@ -47,6 +33,5 @@ public class VersionConfig {
 	public void setVersion(Version version) {
 		this.version = version;
 	}
-	
-	
+
 }
