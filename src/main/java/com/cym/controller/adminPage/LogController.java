1df78dc4d6fabbedc4b1b77d44d4ffe966e20681
cym1102 <cym1102@qq.com>
2020-06-04 22:49:18 +0800

    1

---
+++ src/main/java/com/cym/controller/adminPage/LogController.java
@@ -0,0 +1,87 @@
+package com.cym.controller.adminPage;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cym.model.Log;
+import com.cym.service.SettingService;
+import com.cym.utils.BaseController;
+import com.cym.utils.JsonResult;
+
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
+
+@Controller
+@RequestMapping("/adminPage/log")
+public class LogController extends BaseController {
+	@Autowired
+	SettingService settingService;
+
+	@RequestMapping("")
+	public ModelAndView index(HttpSession httpSession, ModelAndView modelAndView) {
+		List<Log> logList = getLogList();
+
+		CollectionUtil.sort(logList, new Comparator<Log>() {
+
+			@Override
+			public int compare(Log o1, Log o2) {
+				return StrUtil.compare(o2.getTime(), o1.getTime(), true);
+			}
+		});
+
+		modelAndView.addObject("logList", logList);
+		modelAndView.setViewName("/adminPage/log/index");
+		return modelAndView;
+	}
+
+	private List<Log> getLogList() {
+		List<Log> list = new ArrayList<Log>();
+
+		File dir = new File("/home/nginxWebUI/log/");
+
+		File[] fileList = dir.listFiles();
+		for (File file : fileList) {
+			if (file.getName().contains("access")) {
+				Log log = new Log();
+				log.setPath(file.getPath().replace("\\", "/"));
+				DateTime date = DateUtil.parse(file.getName().replace("access.log", ""), "yyyy-MM-dd_HH-mm-ss");
+				log.setTime(DateUtil.format(date, "yyyy-MM-dd HH:mm:ss"));
+
+				list.add(log);
+			}
+		}
+
+		return list;
+	}
+
+	@RequestMapping("del")
+	@ResponseBody
+	public JsonResult del(String path) {
+		FileUtil.del(path);
+		return renderSuccess();
+	}
+	
+	@RequestMapping("setLog")
+	@ResponseBody
+	public JsonResult setLog(Boolean logStatus) {
+
+		settingService.set("logStatus", logStatus.toString());
+		
+		
+		return renderSuccess();
+	}
+
+}
---
+++ src/main/java/com/cym/model/LogInfo.java
@@ -0,0 +1,113 @@
+package com.cym.model;
+
+public class LogInfo {
+	String remoteAddr;
+	String remoteUser;
+	String timeLocal;
+	String request;
+	String httpHost;
+	String status;
+	String requestLength;
+	String bodyBytesDent;
+	String httpReferer;
+	String httpUserAgent;
+	String requestTime;
+	String upstreamResponseTime;
+
+	public String getRemoteAddr() {
+		return remoteAddr;
+	}
+
+	public void setRemoteAddr(String remoteAddr) {
+		this.remoteAddr = remoteAddr;
+	}
+
+	public String getRemoteUser() {
+		return remoteUser;
+	}
+
+	public void setRemoteUser(String remoteUser) {
+		this.remoteUser = remoteUser;
+	}
+
+	public String getTimeLocal() {
+		return timeLocal;
+	}
+
+	public void setTimeLocal(String timeLocal) {
+		this.timeLocal = timeLocal;
+	}
+
+	public String getRequest() {
+		return request;
+	}
+
+	public void setRequest(String request) {
+		this.request = request;
+	}
+
+	public String getHttpHost() {
+		return httpHost;
+	}
+
+	public void setHttpHost(String httpHost) {
+		this.httpHost = httpHost;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getRequestLength() {
+		return requestLength;
+	}
+
+	public void setRequestLength(String requestLength) {
+		this.requestLength = requestLength;
+	}
+
+	public String getBodyBytesDent() {
+		return bodyBytesDent;
+	}
+
+	public void setBodyBytesDent(String bodyBytesDent) {
+		this.bodyBytesDent = bodyBytesDent;
+	}
+
+	public String getHttpReferer() {
+		return httpReferer;
+	}
+
+	public void setHttpReferer(String httpReferer) {
+		this.httpReferer = httpReferer;
+	}
+
+	public String getHttpUserAgent() {
+		return httpUserAgent;
+	}
+
+	public void setHttpUserAgent(String httpUserAgent) {
+		this.httpUserAgent = httpUserAgent;
+	}
+
+	public String getRequestTime() {
+		return requestTime;
+	}
+
+	public void setRequestTime(String requestTime) {
+		this.requestTime = requestTime;
+	}
+
+	public String getUpstreamResponseTime() {
+		return upstreamResponseTime;
+	}
+
+	public void setUpstreamResponseTime(String upstreamResponseTime) {
+		this.upstreamResponseTime = upstreamResponseTime;
+	}
+
+}
