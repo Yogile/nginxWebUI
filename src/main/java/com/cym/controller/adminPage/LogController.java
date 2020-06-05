74814d6fc85f4d42b1dd3052c60f29283988671e
cym1102 <cym1102@qq.com>
2020-06-05 11:53:41 +0800

    1

--- src/main/java/com/cym/controller/adminPage/LogController.java
+++ src/main/java/com/cym/controller/adminPage/LogController.java
@@ -57,7 +57,7 @@ public class LogController extends BaseController {
 			if (file.getName().contains("access") && !file.getName().equals("access.log")) {
 				Log log = new Log();
 				log.setPath(file.getPath().replace("\\", "/"));
-				DateTime date = DateUtil.parse(file.getName().replace("access.log", ""), "yyyy-MM-dd_HH-mm-ss");
+				DateTime date = DateUtil.parse(file.getName().replace("access.", "").replace(".log", ""), "yyyy-MM-dd_HH-mm-ss");
 				log.setTime(DateUtil.format(date, "yyyy-MM-dd HH:mm:ss"));
 
 				list.add(log);
