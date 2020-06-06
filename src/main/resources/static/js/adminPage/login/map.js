23f1d88248d2f8aa3928e46d64a1aaab69e4808a
cym1102 <cym1102@qq.com>
2020-06-06 13:31:38 +0800

    1

--- src/main/resources/static/js/adminPage/login/map.js
+++ src/main/resources/static/js/adminPage/login/map.js
@@ -1,15 +1,37 @@
-$(function(){
+$(function() {
 	var map = new AMap.Map('container', {
-	    zoom: 15,
-	    center:[104.066422,30.65577],
-	}); 
-	var wms  = new AMap.TileLayer.WMTS({
-	     url: 'http://t0.tianditu.gov.cn/img_c/wmts?tk=26837ca6679a927b7877c0f0aa01407c',        
-	     blend:false,
-	     tileSize:256,
-	     params:{'Layer': '0',Version:'1.0.0',Format: 'image/png'}
+		zoom : 10,
+		center : [ 104.066422, 30.65577 ],
+	});
+	var wms = new AMap.TileLayer.WMTS({
+		url : 'http://t0.tianditu.gov.cn/img_w/wmts',
+		blend : false,
+		tileSize : 256,
+		params : {
+			LAYER : 'img',
+			VERSION : '1.0.0',
+			Format : 'tiles',
+			TileMatrixSet : 'w',
+			STYLE : 'default',
+			tk : '26837ca6679a927b7877c0f0aa01407c'
+		}
 	})
 	wms.setMap(map)
+
 	
+//	var wms = new AMap.TileLayer.WMTS({
+//		url : 'https://www.google.cn/maps/vt',
+//		blend : false,
+//		tileSize : 256,
+//		params : {
+//			lyrs : 's',
+//			gl : 'cn',
+//			Layer : 'img',
+//			Version : '1.0.0',
+//			Format : 'tiles'
+//		}
+//	})
+//	wms.setMap(map)
+
 	
 })
