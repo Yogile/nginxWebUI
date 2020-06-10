06386e55a7192587b3100eee469476582661652d
cym1102 <cym1102@qq.com>
2020-06-10 15:36:20 +0800

    1

--- src/main/resources/static/js/adminPage/login/map.js
+++ src/main/resources/static/js/adminPage/login/map.js
@@ -1,10 +1,10 @@
 $(function() {
-	var imageLayer = new AMap.ImageLayer({
-		url : ctx + '/img/mp.png',
-		bounds : new AMap.Bounds([ 120.081730, 35.908096 ], [ 120.083030, 35.909396 ]),
-		zooms : [ 3, 20 ],
-		zIndex : 3,
-	});
+//	var imageLayer = new AMap.ImageLayer({
+//		url : ctx + '/img/mp.png',
+//		bounds : new AMap.Bounds([ 120.081730, 35.908096 ], [ 120.083030, 35.909396 ]),
+//		zooms : [ 3, 20 ],
+//		zIndex : 3,
+//	});
 
 	var map = new AMap.Map('container', {
 		zoom : 20,
@@ -13,7 +13,7 @@ $(function() {
 		viewMode : '3D',
 		pitch : 40,
 		expandZoomRange : true,
-		layers : [ new AMap.TileLayer(), imageLayer ]
+//		layers : [ new AMap.TileLayer(), imageLayer ]
 	});
 
 	var googleLayer = new AMap.TileLayer({
@@ -25,10 +25,10 @@ $(function() {
 
 	googleLayer.setMap(map);
 
-	var mouseTool = new AMap.MouseTool(map); // 在地图中添加MouseTool插件
-	var drawRectangle = mouseTool.rectangle(); // 用鼠标工具画矩形
-	AMap.event.addListener(mouseTool, 'draw', function(e) { // 添加事件
-		console.log(e.obj.getPath());// 获取路径
-		alert(e.obj.getPath());
-	});
+//	var mouseTool = new AMap.MouseTool(map); // 在地图中添加MouseTool插件
+//	var drawRectangle = mouseTool.rectangle(); // 用鼠标工具画矩形
+//	AMap.event.addListener(mouseTool, 'draw', function(e) { // 添加事件
+//		console.log(e.obj.getPath());// 获取路径
+//		alert(e.obj.getPath());
+//	});
 })
