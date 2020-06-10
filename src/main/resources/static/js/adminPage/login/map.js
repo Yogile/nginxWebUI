43bee52253466a19922bb8d3fdd2e822b171089a
cym1102 <cym1102@qq.com>
2020-06-10 16:29:18 +0800

    1

--- src/main/resources/static/js/adminPage/login/map.js
+++ src/main/resources/static/js/adminPage/login/map.js
@@ -1,7 +1,7 @@
 $(function() {
 //	var imageLayer = new AMap.ImageLayer({
 //		url : ctx + '/img/mp.png',
-//		bounds : new AMap.Bounds([ 120.081730, 35.908096 ], [ 120.083030, 35.909396 ]),
+//		bounds : new AMap.Bounds([120.080232,35.908669], [120.0828,35.907638]),
 //		zooms : [ 3, 20 ],
 //		zIndex : 3,
 //	});
@@ -9,7 +9,7 @@ $(function() {
 	var map = new AMap.Map('container', {
 		zoom : 20,
 		zooms : [ 3, 20 ],
-		center : [ 120.081730, 35.908096 ],
+		center : [ 120.080232,35.908669],
 		viewMode : '3D',
 		pitch : 0,
 		expandZoomRange : true,
@@ -29,6 +29,7 @@ $(function() {
 	var drawRectangle = mouseTool.rectangle(); // 用鼠标工具画矩形
 	AMap.event.addListener(mouseTool, 'draw', function(e) { // 添加事件
 		console.log(e.obj.getPath());// 获取路径
-		alert(e.obj.getPath());
+		//layer.msg(e.obj.getPath());
+		$("#text").html(JSON.stringify( e.obj.getPath()));
 	});
 })
