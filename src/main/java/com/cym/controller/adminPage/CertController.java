d0089dab39e94790a2d1c4b3c1612a1fa54b6604
cym1102 <cym1102@qq.com>
2020-06-18 11:15:30 +0800

    1

--- src/main/java/com/cym/controller/adminPage/CertController.java
+++ src/main/java/com/cym/controller/adminPage/CertController.java
@@ -173,6 +173,11 @@ public class CertController extends BaseController {
 		}
 		isInApply = true;
 		
+		// 如果在容器中, 要去替换主nginx.conf, 而不是/home/nginxWebUI/nginx.conf
+		if(nginxPath.contains(InitConfig.home)) {
+			nginxPath = "/etc/nginx/nginx.conf";
+		}
+				
 		// 替换nginx.conf并重启
 		replaceStartNginx(nginxPath, cert.getDomain());
 		String rs = "";
@@ -187,6 +192,7 @@ public class CertController extends BaseController {
 			rs = e.getMessage();
 		}
 
+		// 还原nginx.conf并重启
 		backupStartNginx(nginxPath);
 		if (rs.contains("Cert success")) {
 			String certDir = "/root/.acme.sh/" + cert.getDomain() + "/";
