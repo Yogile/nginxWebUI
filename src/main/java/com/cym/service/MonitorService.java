6dde8619237bc39780155afeafb807c8bd95a6ef
cym1102 <cym1102@qq.com>
2020-07-02 10:09:38 +0800

    1

--- src/main/java/com/cym/service/MonitorService.java
+++ src/main/java/com/cym/service/MonitorService.java
@@ -54,47 +54,14 @@ public class MonitorService {
 		double mem = usedMemory / totalMemorySize;
 		// 构造返回对象
 		MonitorInfo infoBean = new MonitorInfo();
-		infoBean.setFreePhysicalMemorySize(NumberUtil.decimalFormat("#.00GB", freePhysicalMemorySize));
-		infoBean.setTotalMemorySize(NumberUtil.decimalFormat("#.00GB", totalMemorySize));
-		infoBean.setUsedMemory(NumberUtil.decimalFormat("#.00GB", usedMemory));
+		infoBean.setFreePhysicalMemorySize(NumberUtil.decimalFormat("0.00GB", freePhysicalMemorySize));
+		infoBean.setTotalMemorySize(NumberUtil.decimalFormat("0.00GB", totalMemorySize));
+		infoBean.setUsedMemory(NumberUtil.decimalFormat("0.00GB", usedMemory));
 		infoBean.setCpuRatio(NumberUtil.decimalFormat("#.##%", cpu));
 		infoBean.setMemRatio(NumberUtil.decimalFormat("#.##%", mem));
 
-		List<Map<String, String>> list = new ArrayList<Map<String, String>>();
-
-		
 		return infoBean;
 	}
+	
 
-	/** */
-	/**
-	 * 测试方法.
-	 * 
-	 * @param args
-	 * @throws Exception
-	 * @author amg * Creation date: 2008-4-30 - 下午04:47:29
-	 */
-	public static void main(String[] args) throws Exception {
-
-		List<Map<String, String>> list = new ArrayList<Map<String, String>>();
-
-		File[] roots = File.listRoots();// 获取磁盘分区列表
-		for (File file : roots) {
-			Map<String, String> map = new HashMap<String, String>();
-
-			long freeSpace = file.getFreeSpace();
-			long totalSpace = file.getTotalSpace();
-			long usableSpace = totalSpace - freeSpace;
-
-			map.put("path", file.getPath());
-			map.put("freeSpace", freeSpace / 1024 / 1024 / 1024 + "G");// 空闲空间
-			map.put("usableSpace", usableSpace / 1024 / 1024 / 1024 + "G");// 可用空间
-			map.put("totalSpace", totalSpace / 1024 / 1024 / 1024 + "G");// 总空间
-			map.put("percent", NumberUtil.decimalFormat("#.##%", (double) usableSpace / (double) totalSpace));// 总空间
-
-			list.add(map);
-		}
-		System.out.println(list);
-
-	}
 }
