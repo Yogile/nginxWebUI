6083f508c069486e57a0c0729dcfdbef1a1b18dc
cym1102 <cym1102@qq.com>
2020-07-08 18:52:35 +0800

    1

--- src/main/java/com/cym/service/ServerService.java
+++ src/main/java/com/cym/service/ServerService.java
@@ -149,8 +149,12 @@ public class ServerService {
 
 	}
 
-	public boolean hasListen(String listen) {
-		return sqlHelper.findCountByQuery(new ConditionAndWrapper().eq("listen", listen), Server.class) > 0;
+	public boolean hasListen(String listen, String serverId) {
+		ConditionAndWrapper conditionAndWrapper =  new ConditionAndWrapper().eq("listen", listen);
+		if(StrUtil.isNotEmpty(serverId)) {
+			conditionAndWrapper.ne("id", serverId);
+		}
+		return sqlHelper.findCountByQuery(conditionAndWrapper, Server.class) > 0;
 	}
 
 }
