69c1628581b31653bc77d9abb5f9476f242c31a7
cym1102 <cym1102@qq.com>
2020-07-10 13:56:53 +0800

    1

--- src/main/java/com/cym/config/ScheduleTask.java
+++ src/main/java/com/cym/config/ScheduleTask.java
@@ -1,26 +1,38 @@
 package com.cym.config;
 
-import cn.craccd.sqlHelper.utils.SqlHelper;
-import cn.hutool.core.date.DateTime;
-import cn.hutool.core.date.DateUtil;
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.util.ZipUtil;
-import com.cym.controller.adminPage.CertController;
-import com.cym.controller.adminPage.ConfController;
-import com.cym.model.Cert;
-import com.cym.service.LogService;
-import com.cym.service.SettingService;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import javax.annotation.PostConstruct;
-import java.io.File;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
+import com.cym.controller.adminPage.CertController;
+import com.cym.controller.adminPage.ConfController;
+import com.cym.controller.adminPage.RemoteController;
+import com.cym.model.Cert;
+import com.cym.model.Remote;
+import com.cym.service.LogService;
+import com.cym.service.SettingService;
+import com.cym.utils.SendCloudUtils;
+
+import cn.craccd.sqlHelper.utils.SqlHelper;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.lang.TypeReference;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.util.ZipUtil;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
 
 @Configuration // 1.主要用于标记配置类，兼备Component的效果。
 @EnableScheduling // 2.开启定时任务
@@ -30,14 +42,19 @@ public class ScheduleTask {
 	final CertController certController;
 	final SettingService settingService;
 	final ConfController confController;
+	final RemoteController remoteController;
 	final LogService logInfoService;
+	final SendCloudUtils smCloudUtils;
 
-	public ScheduleTask(SqlHelper sqlHelper, CertController certController, SettingService settingService, ConfController confController, LogService logInfoService) {
+	public ScheduleTask(SendCloudUtils smCloudUtils, RemoteController remoteController, SqlHelper sqlHelper, CertController certController, SettingService settingService,
+			ConfController confController, LogService logInfoService) {
 		this.sqlHelper = sqlHelper;
 		this.certController = certController;
 		this.settingService = settingService;
 		this.confController = confController;
 		this.logInfoService = logInfoService;
+		this.remoteController = remoteController;
+		this.smCloudUtils = smCloudUtils;
 	}
 
 	@PostConstruct
@@ -90,11 +107,42 @@ public class ScheduleTask {
 
 		);
 	}
-	
+
 	// 检查nginx运行
 	@Scheduled(cron = "0 0/5 * * * ?")
 	public void nginxTasks() {
-		
+
+		String lastSend = settingService.get("lastSend");
+		String mail = settingService.get("mail");
+		String nginxMonitor = settingService.get("nginxMonitor");
+		if ("true".equals(nginxMonitor) && StrUtil.isNotEmpty(mail) && (StrUtil.isEmpty(lastSend) || System.currentTimeMillis() - Long.parseLong(lastSend) > TimeUnit.HOURS.toMillis(1))) {
+
+			List<Remote> remoteList = sqlHelper.findAll(Remote.class);
+
+			List<String> names = new ArrayList<>();
+			for (Remote remote : remoteList) {
+				try {
+					String json = HttpUtil.get(remote.getProtocol() + "://" + remote.getIp() + ":" + remote.getPort() + "/adminPage/remote/version?creditKey=" + remote.getCreditKey(), 1000);
+					Map<String, Object> map = JSONUtil.toBean(json, new TypeReference<Map<String, Object>>() {
+					}.getType(), false);
+
+					if ((Integer) map.get("nginx") == 0) {
+						names.add(remote.getDescr() + "(" + remote.getIp() + ":" + remote.getPort() + ")");
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+					names.add(remote.getDescr() + "(" + remote.getIp() + ":" + remote.getPort() + ")");
+				}
+			}
+
+			Map<String, Object> map = remoteController.version();
+			if ((Integer) map.get("nginx") == 0) {
+				names.add("本机");
+			}
+
+			smCloudUtils.sendMail(mail, StrUtil.join(" ", names));
+		}
+
 	}
 
 }
