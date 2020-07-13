c2b3b9981e7d68fbff04533b22a41b21d923b80b
cym1102 <cym1102@qq.com>
2020-07-13 21:13:39 +0800

    1

--- src/main/java/com/cym/config/ScheduleTask.java
+++ src/main/java/com/cym/config/ScheduleTask.java
@@ -74,9 +74,8 @@ public class ScheduleTask {
 		System.out.println("检查需要续签的证书");
 		long time = System.currentTimeMillis();
 		for (Cert cert : certList) {
-			if (cert.getMakeTime() != null && cert.getAutoRenew() == 1 && time - cert.getMakeTime() > TimeUnit.DAYS.toMillis(59)) {
-				System.out.println(DateUtil.format(new Date(), "yyyy-MM-dd HH:mm:ss") + " 开始续签证书:" + cert.getDomain());
-				// 大于60天的续签
+			// 大于50天的续签
+			if (cert.getMakeTime() != null && cert.getAutoRenew() == 1 && time - cert.getMakeTime() > TimeUnit.DAYS.toMillis(50)) {
 				certController.apply(cert.getId(), "renew");
 			}
 		}
