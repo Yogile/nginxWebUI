4e9af53e63302bdac43ae9b506b8cc84cf703b5c
cym1102 <cym1102@qq.com>
2020-08-31 20:55:05 +0800

    123

--- src/main/java/com/cym/service/RemoteService.java
+++ src/main/java/com/cym/service/RemoteService.java
@@ -23,12 +23,12 @@ public class RemoteService {
 	@Autowired
 	SqlHelper sqlHelper;
 
-	public void getCreditKey(Remote remote) {
+	public void getCreditKey(Remote remote, String code) {
 		Map<String, Object> paramMap = new HashMap<String, Object>();
 
 		paramMap.put("name", remote.getName());
 		paramMap.put("pass", remote.getPass());
-
+		paramMap.put("code", code);
 		try {
 			String rs = HttpUtil.post(remote.getProtocol() + "://" + remote.getIp() + ":" + remote.getPort() + "/adminPage/login/getCredit", paramMap, 2000);
 
