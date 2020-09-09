0bcd4facb9fb1cab7ee554efbef9f9a7b90666fc
cym1102 <cym1102@qq.com>
2020-09-09 11:20:06 +0800

    123

--- docker/nginxWebUI.sh
+++ docker/nginxWebUI.sh
@@ -4,7 +4,7 @@ if [ ! -f "/home/nginxWebUI/nginxWebUI-${version}.jar" ]; then
     if [ ! -d "/home/nginxWebUI/" ]; then
        mkdir /home/nginxWebUI/
     fi
-       mv /home/nginxWebUI-${version}.jar /home/nginxWebUI/
+    mv /home/nginxWebUI-*.jar /home/nginxWebUI/
 fi
 
 echo "执行文件：$0";
