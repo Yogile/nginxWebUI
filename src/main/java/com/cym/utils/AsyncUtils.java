0ff90e5cb834b6da8265de4cd309d56e0a1a2b41
cym1102 <cym1102@qq.com>
2020-10-02 16:05:33 +0800

    123

--- src/main/java/com/cym/utils/AsyncUtils.java
+++ src/main/java/com/cym/utils/AsyncUtils.java
@@ -1,14 +1,11 @@
 package com.cym.utils;
 
-import java.io.File;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
-import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.RuntimeUtil;
 
@@ -24,17 +21,6 @@ public class AsyncUtils {
 	public void run(String path) {
 		ThreadUtil.safeSleep(2000);
 
-		System.out.println(path);
-		File file = new File(path);
-		String[] list = file.getParentFile().list();
-		for (String f : list) {
-			System.out.println(f);
-			if (f.startsWith("nginxWebUI") && f.endsWith(".jar")) {
-				System.err.println("del " + f);
-				FileUtil.del(f);
-			}
-		}
-
 		String cmd = "mv " + path + " " + path.replace(".update", "");
 		LOG.info(cmd);
 		RuntimeUtil.exec(cmd);
@@ -44,16 +30,5 @@ public class AsyncUtils {
 		RuntimeUtil.exec(cmd);
 
 	}
-	
-	public static void main(String[] args) {
-		File file = new File("E:\\home\\test.gz");
-		String[] list = file.getParentFile().list();
-		for (String f : list) {
-			System.out.println(f);
-			if (f.startsWith("nginxWebUI") && f.endsWith(".jar")) {
-				System.err.println("del " + f);
-				FileUtil.del(f);
-			}
-		}
-	}
+
 }
