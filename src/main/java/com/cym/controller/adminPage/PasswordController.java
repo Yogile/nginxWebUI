9ce659622032b3c49f97f0532027cd26dd6f93d3
cym1102 <cym1102@qq.com>
2020-10-05 16:41:15 +0800

    1

---
+++ src/main/java/com/cym/controller/adminPage/PasswordController.java
@@ -0,0 +1,68 @@
+package com.cym.controller.adminPage;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cym.model.Password;
+import com.cym.service.PasswordService;
+import com.cym.utils.BaseController;
+import com.cym.utils.JsonResult;
+
+import cn.craccd.sqlHelper.bean.Page;
+import cn.hutool.core.util.StrUtil;
+
+@RequestMapping("/adminPage/password")
+@Controller
+public class PasswordController extends BaseController {
+	@Autowired
+	PasswordService passwordService;
+
+	@RequestMapping("")
+	public ModelAndView index(HttpSession httpSession, ModelAndView modelAndView, Page page) {
+		page = passwordService.search(page);
+
+		modelAndView.addObject("page", page);
+		modelAndView.setViewName("/adminPage/password/index");
+		return modelAndView;
+	}
+
+	@RequestMapping("addOver")
+	@ResponseBody
+	public JsonResult addOver(Password password) {
+		
+		if (StrUtil.isEmpty(password.getId())) {
+			Long count = passwordService.getCountByName(password.getName());
+			if (count > 0) {
+				return renderError(m.get("adminStr.nameRepetition"));
+			}
+		} else {
+			Long count = passwordService.getCountByNameWithOutId(password.getName(), password.getId());
+			if (count > 0) {
+				return renderError(m.get("adminStr.nameRepetition"));
+			}
+		}
+		
+		sqlHelper.insertOrUpdate(password);
+
+		return renderSuccess();
+	}
+
+	@RequestMapping("del")
+	@ResponseBody
+	public JsonResult del(String id) {
+		sqlHelper.deleteById(id, Password.class);
+
+		return renderSuccess();
+	}
+
+	@RequestMapping("detail")
+	@ResponseBody
+	public JsonResult detail(String id) {
+		return renderSuccess(sqlHelper.findById(id, Password.class));
+	}
+}
