641842cabe65255ef9f2179ce0148bfc55d71dce
cym1102 <cym1102@qq.com>
2020-11-19 17:31:24 +0800

    1

--- src/main/resources/static/js/adminPage/upstream/index.js
+++ src/main/resources/static/js/adminPage/upstream/index.js
@@ -378,8 +378,7 @@ function upstreamMonitor() {
 
 
 function upstreamOver() {
-	var myreg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
-	if ($("#mail").val() == '' || !myreg.test($("#mail").val())) {
+	if ($("#mail").val().indexOf("@") == -1) {
 		layer.alert(remoteStr.emailTips);
 		return;
 	}
